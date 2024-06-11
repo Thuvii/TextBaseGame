6bc3668e01c0f89b9309dd52f015c249c4ceab6f
ThuVi <buihhh321@gmail.com>
2024-06-11 01:51:07 -0400

    implement attack

--- TextBaseGame/Encounters.cs
+++ TextBaseGame/Encounters.cs
@@ -4,8 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TextBaseGame {
-    public class Encounters {
+namespace TextBaseGame
+{
+    public class Encounters
+    {
+        static Random rand = new Random();
         //Encounter Generic
+
+        //Encounter
+        public static void FirstEncounter()
+        {
+            Console.WriteLine("You throw open the door and grab a rusty metal sword\n" +
+            "while charging toward the captor.\n" +
+            "He turns...");
+            Console.ReadKey();
+        }
+
+
+        //encounter tools
+        public static void Combat(bool random, string name, int power, int health)
+        {
+            string n = "";
+            int p = 0;
+            int h = 0;
+            if (random)
+            {
+
+            }
+            else
+            {
+                n = name;
+                p = power;
+                h = health;
+            }
+            while (h > 0)
+            {
+                Console.Clear();
+                Console.WriteLine("========================");
+                Console.WriteLine("|  (A)ttack  (D)efend  |");
+                Console.WriteLine("|   (R)un     (H)eal   |");
+                Console.WriteLine("========================" +);
+                Console.WriteLine("  Potion: " + Program.player.potion + "  Health: " + Program.player.health);
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "a" || userInput.ToLower() == "attack")
+                {
+                    //acttack
+                    Console.WriteLine("With haste you surge forth, your sword flying in your hand!\n" +
+                    "As you pass." + n + "strikes you back!!!");
+                    int damage = power - Program.player.amorValue;
+                    int attack = rand.Next(0, Program.player.weaponValue) + rand.Next(1, 4);
+                    Console.WriteLine("You lose" + damage + "health and deal " + attack + " damage");
+                    Program.player.health -= damage;
+                    h -= attack;
+                }
+                else if (userInput.ToLower() == "d" || userInput.ToLower() == "defend")
+                {
+                    //defend
+                }
+                else if (userInput.ToLower() == "r" || userInput.ToLower() == "run")
+                {
+                    //defend
+                }
+                else if (userInput.ToLower() == "h" || userInput.ToLower() == "heal")
+                {
+                    //defend
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input! Please enter A, D, R, or H.");
+                    continue;
+                }
+            }
+        }
     }
 }
--- TextBaseGame/Player.cs
+++ TextBaseGame/Player.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TextBaseGame {
-    public class Player {
+namespace TextBaseGame
+{
+    public class Player
+    {
         public string name;
-        public int coins =0;
+        public int coins = 0;
         public int health = 10;
         public int damage = 1;
         public int amorValue = 0;
         public int potion = 5;
 
+        public int weaponValue = 1;
     }
 }
